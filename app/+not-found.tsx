859caef709f45e476df233abba9e4b55fceb1428
Chao Lobi <gogoiladitya@gmail.com>
2024-07-12 12:06:36 +0530

    initial setup

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,17 +1,15 @@
-import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
-
-import { Text, View } from '@/components/Themed';
+import { Link, Stack } from "expo-router";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function NotFoundScreen() {
   return (
     <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
+      <Stack.Screen options={{ title: "Oops!" }} />
       <View style={styles.container}>
         <Text style={styles.title}>This screen doesn't exist.</Text>
 
-        <Link href="/" style={styles.link}>
-          <Text style={styles.linkText}>Go to home screen!</Text>
+        <Link href="/onboard" style={styles.link}>
+          <Text style={styles.linkText}>Go to Onboard Screen!</Text>
         </Link>
       </View>
     </>
@@ -21,20 +19,22 @@ export default function NotFoundScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     padding: 20,
   },
   title: {
+    fontFamily: "VarelaFont",
     fontSize: 20,
-    fontWeight: 'bold',
   },
   link: {
+    fontFamily: "VarelaFont",
     marginTop: 15,
     paddingVertical: 15,
   },
   linkText: {
-    fontSize: 14,
-    color: '#2e78b7',
+    fontFamily: "VarelaFont",
+    fontSize: 16,
+    color: "#2e78b7",
   },
 });
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,39 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
+const tintColorLight = "#2f95dc";
+const tintColorDark = "#fff";
 
 export default {
   light: {
-    text: '#000',
-    background: '#fff',
+    200: "#E8E9E9",
+    text: "#000",
+    background: "#fff",
     tint: tintColorLight,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#ccc",
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#fff',
-    background: '#000',
+    200: "#0D0F10",
+    300: "#131619",
+    400: "#1A1D21",
+    500: "#363A3D",
+    600: "#76828D",
+    700: "#ABB8C4",
+    text: "#fff",
+    background: "#000",
     tint: tintColorDark,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#ccc",
     tabIconSelected: tintColorDark,
+  },
+  green: {
+    500: "#24AE7C",
+    600: "#0D2A1F",
+  },
+  blue: {
+    500: "#79B5EC",
+    600: "#152432",
+  },
+  red: {
+    500: "#F37877",
+    600: "#3E1716",
+    700: "#F24E43",
   },
 };
